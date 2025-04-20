17ee314fe9e21e040441676b70b3704a2cab4540
DILEPE <tu_email@example.com>
2025-04-19 22:44:19 -0500

    Controlador ProductoController con CRUD básico

--- ProductosAPI/Program.cs
+++ ProductosAPI/Program.cs
@@ -1,25 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ProductosAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración del DbContext con la cadena de conexión desde appsettings.json
+builder.Services.AddDbContext<ProductoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductoDb")));
 
+// Agregar otros servicios como los controladores, etc.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+// Configuración de la tubería de solicitudes HTTP
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
